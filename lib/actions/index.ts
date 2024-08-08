050b7ac1286f972685e4462921b91ad46177f9f3
Reghardt Pienaar <reghardt7@gmail.com>
2024-08-08 08:38:19 +0200

    ravalidate 60

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,5 +1,5 @@
 export async function getPosts() {
-  const res = await fetch('https://public-api.wordpress.com/rest/v1.1/sites/reghardtpienaar.wordpress.com/posts', { next: { revalidate: 3600 } });
+  const res = await fetch('https://public-api.wordpress.com/rest/v1.1/sites/reghardtpienaar.wordpress.com/posts', { next: { revalidate: 60 } });
   if (!res.ok) throw new Error('Failed to fetch posts');
   const data = await res.json();
   return data.posts;
